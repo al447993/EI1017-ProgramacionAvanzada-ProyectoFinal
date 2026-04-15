c4d4fd906d879e1160fc10b84086bf5fce87d29e
al439579 <al439579@uji.es>
2026-04-15 15:26:59 +0200

    Arreglo del test Suite

--- src/main/java/es/uji/al447993/clasificarFlores/tests/TestSuite.java
+++ src/main/java/es/uji/al447993/clasificarFlores/tests/TestSuite.java
@@ -1,8 +1,4 @@
-//COMENTARIO: no hemos conseguido que funcione, hemos arrancado
-//todos los tests a la vez a través de la carpeta que los contiene
-//(Click derecho en carpeta tests - RUN Tests in 'es.uji.al447993.clasificarFlores.tests')
-
-/*package es.uji.al447993.clasificarFlores.tests;
+package es.uji.al447993.clasificarFlores.tests;
 
 import org.junit.platform.suite.api.IncludeClassNamePatterns;
 import org.junit.platform.suite.api.SelectPackages;
@@ -13,9 +9,9 @@ import org.junit.platform.suite.api.SuiteDisplayName;
 
 @Suite
 @SuiteDisplayName("Lanzar todos los tests")
-@SelectPackages({"es.uji.al447993.clasificarFlores.KNN", "es.uji.al447993.clasificarFlores.CSV;", "es.uji.al447993.clasificarFlores.Table"})
+@SelectPackages({"es.uji.al447993.clasificarFlores.tests"})
 @IncludeClassNamePatterns(".*Test")
 public class TestSuite {
 }
-*/
+
 
