78afa819830d25921ceade7201443dae82a63fa9
al447993 <al447993@uji.es>
2026-04-21 11:23:40 +0200

    Delete src/main/java/es/uji/al447993/clasificarFloresRamos directory
    Eliminación paquete clonado

--- src/main/java/es/uji/al447993/clasificarFloresRamos/interfaces/Distance.java
+++
@@ -1,8 +0,0 @@
-package es.uji.al447993.clasificarFloresRamos.interfaces;
-
-import java.util.List;
-
-public interface Distance {
-
-    double calculateDistance(List<Double> p, List<Double> q);
-}
